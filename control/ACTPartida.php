761cf200cc51143a85888bd272e16fcc7e359e9a
Jaime Rivera <jaime@kplian.com>
2013-05-31 16:48:09 -0400

    anadiendo filtro de partida por concepto de gasto

--- control/ACTPartida.php
+++ control/ACTPartida.php
@@ -16,6 +16,10 @@ class ACTPartida extends ACTbase{
 	    	$this->objParam->addFiltro("par.id_gestion = ".$this->objParam->getParametro('id_gestion'));	
 		}
 		
+		if($this->objParam->getParametro('id_concepto_ingas')!=''){
+	    	$this->objParam->addFiltro("par.id_partida in (select id_partida from pre.tconcepto_partida cp where cp.id_concepto_ingas = " . $this->objParam->getParametro('id_concepto_ingas') . ")");	
+		}
+		
 		
 		if($this->objParam->getParametro('tipo')!=''){
 	    	$this->objParam->addFiltro("par.tipo = ''".$this->objParam->getParametro('tipo')."''");	
@@ -24,11 +28,7 @@ class ACTPartida extends ACTbase{
 		
 		if($this->objParam->getParametro('sw_transaccional')!=''){
 	    	$this->objParam->addFiltro("par.sw_transaccional = ''".$this->objParam->getParametro('sw_transaccional')."''");	
-		}
-		
-		
-		
-		
+		}		
 
 		$this->objParam->defecto('dir_ordenacion','asc');
 		if($this->objParam->getParametro('tipoReporte')=='excel_grid' || $this->objParam->getParametro('tipoReporte')=='pdf_grid'){
