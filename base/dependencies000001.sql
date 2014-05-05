6492c0275cc3ce9290a22f710bb40762528b2f28
dora <dcastro@boa.bo>
2014-05-05 09:27:00 -0400

    cambios en datos de seguridad

--- base/dependencies000001.sql
+++ base/dependencies000001.sql
@@ -35,3 +35,70 @@ ALTER TABLE pre.tpresupuesto
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /***********************************F-DEP-GSS-PRE-0-26/02/2013*****************************************/
+
+/***********************************I-DEP-JRR-PRE-0-24/04/2014*****************************************/
+select pxp.f_insert_testructura_gui ('CINGAS.1', 'CINGAS');
+select pxp.f_insert_testructura_gui ('CINGAS.2', 'CINGAS');
+select pxp.f_insert_testructura_gui ('CINGAS.3', 'CINGAS');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_INS', 'PRE.1.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_MOD', 'PRE.1.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_ELI', 'PRE.1.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_ARB_SEL', 'PRE.1.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_GES_SEL', 'PRE.1.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CEC_SEL', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CECCOM_SEL', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CECCOMFU_SEL', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CCFILDEP_SEL', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PRE_INS', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PRE_MOD', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PRE_ELI', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PRE_SEL', 'PRE.2.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_ARB_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('CONTA_CTA_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('CONTA_CTA_ARB_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('CONTA_AUXCTA_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CEC_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CECCOM_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CECCOMFU_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CCFILDEP_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_INS', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_MOD', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_ELI', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_GES_SEL', 'CCTA', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CONIG_INS', 'CINGAS', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CONIG_MOD', 'CINGAS', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CONIG_ELI', 'CINGAS', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CONIG_SEL', 'CINGAS', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CONIGPP_SEL', 'CINGAS', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CATCMB_SEL', 'CINGAS', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_SEL', 'CINGAS.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_ARB_SEL', 'CINGAS.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CONP_INS', 'CINGAS.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CONP_MOD', 'CINGAS.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CONP_ELI', 'CINGAS.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CONP_SEL', 'CINGAS.1', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_PAR_ARB_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('CONTA_CTA_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('CONTA_CTA_ARB_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('CONTA_AUXCTA_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CEC_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CECCOM_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CECCOMFU_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CCFILDEP_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_INS', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_MOD', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_ELI', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PRE_CCTA_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_GES_SEL', 'CINGAS.2', 'no');
+select pxp.f_insert_tprocedimiento_gui ('SEG_SUBSIS_SEL', 'CINGAS.3', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_PACATI_SEL', 'CINGAS.3', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CAT_INS', 'CINGAS.3', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CAT_MOD', 'CINGAS.3', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CAT_ELI', 'CINGAS.3', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CAT_SEL', 'CINGAS.3', 'no');
+select pxp.f_insert_tprocedimiento_gui ('PM_CATCMB_SEL', 'CINGAS.3', 'no');
+
+/***********************************F-DEP-JRR-PRE-0-24/04/2014*****************************************/
