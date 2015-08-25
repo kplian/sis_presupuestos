592076a488fa9395969b14be9e6eb2a7297ea215
dora <dcastro@boa.bo>
2015-08-25 14:51:41 -0400

    integracion con presupeustos

--- base/funciones/pre.f_verificar_com_eje_pag.sql
+++ base/funciones/pre.f_verificar_com_eje_pag.sql
@@ -33,42 +33,54 @@ DECLARE
  
   
   v_nombre_funcion  varchar;
+  v_pre_integrar_presupuestos varchar;
 BEGIN
 
-v_nombre_funcion = 'pre.f_verificar_com_eje_pag';
+  v_nombre_funcion = 'pre.f_verificar_com_eje_pag';
 
   v_sincronizar=pxp.f_get_variable_global('sincronizar');
-  
-  IF(v_sincronizar='true')THEN
-  	
-    --si la sincronizacion esta activa busca lso datos en endesis
-    v_conexion:=migra.f_obtener_cadena_conexion();
-    
-    v_consulta:='select  p_comprometido, p_ejecutado, p_pagado from   presto."f_pr_verificar_comprometido3" ('||COALESCE(p_id_partida_ejecucion::varchar,'NULL')||','||COALESCE(p_id_moneda::varchar,'NULL')||')';
-  	
-    if (p_conexion is null) then   
-        select  * into verificado from dblink(v_conexion,v_consulta,true) as (p_comprometido numeric, p_ejecutado numeric, p_pagado numeric);
-    else
-        select  * into verificado from dblink(p_conexion,v_consulta,true) as (p_comprometido numeric, p_ejecutado numeric, p_pagado numeric);
-    end if;
-  
-  --raise exception '% %',v_consulta,v_conexion;
+  v_pre_integrar_presupuestos = pxp.f_get_variable_global('pre_integrar_presupuestos');
   
   
-  ELSE 
-    --TO DO,   si la sincronizacion no esta activa busca en el sistema de presupeusto local en PXP
+  IF v_pre_integrar_presupuestos = 'true' THEN 
   
-     raise exception 'Verificacion   presupuestaria en PXP no implementada';
+      
+      IF(v_sincronizar='true')THEN
+      	
+        --si la sincronizacion esta activa busca lso datos en endesis
+        v_conexion:=migra.f_obtener_cadena_conexion();
+        
+        v_consulta:='select  p_comprometido, p_ejecutado, p_pagado from   presto."f_pr_verificar_comprometido3" ('||COALESCE(p_id_partida_ejecucion::varchar,'NULL')||','||COALESCE(p_id_moneda::varchar,'NULL')||')';
+      	
+        if (p_conexion is null) then   
+            select  * into verificado from dblink(v_conexion,v_consulta,true) as (p_comprometido numeric, p_ejecutado numeric, p_pagado numeric);
+        else
+            select  * into verificado from dblink(p_conexion,v_consulta,true) as (p_comprometido numeric, p_ejecutado numeric, p_pagado numeric);
+        end if;
+      
+      --raise exception '% %',v_consulta,v_conexion;
+      
+      
+      ELSE 
+        --TO DO,   si la sincronizacion no esta activa busca en el sistema de presupeusto local en PXP
+      
+         raise exception 'Verificacion   presupuestaria en PXP no implementada';
+      
+      
+      
+      END IF;
+      
+      ps_comprometido= verificado.p_comprometido;
+      ps_ejecutado= verificado.p_ejecutado;
+      ps_pagado= verificado.p_pagado;
+ ELSE
+     ps_comprometido = 0;
+     ps_ejecutado = 0;
+     ps_pagado = 0;
   
-  
-  
-  END IF;
-  
- 
+ END IF;
 
-   ps_comprometido= verificado.p_comprometido;
-   ps_ejecutado= verificado.p_ejecutado;
-   ps_pagado= verificado.p_pagado;
+   
 
   
   return NEXT;
