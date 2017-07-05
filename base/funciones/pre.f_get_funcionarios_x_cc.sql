b6924052edcdd5d7d0d3a7e7b30d437924fe7da9
GonzaloSarmientoSejas <gonzalo.sarmiento@boa.bo>
2017-07-05 09:37:42 -0400

    presupuesto servicio

--- base/funciones/pre.f_get_funcionarios_x_cc.sql
+++ base/funciones/pre.f_get_funcionarios_x_cc.sql
@@ -12,13 +12,13 @@ $body$
  DESCRIPCION:   funcion que retorna los funcionarios de un centro de costo
  AUTOR: 		Gonzalo Sarmiento Sejas
  FECHA:	        15-05-2017
- COMENTARIOS:	
+ COMENTARIOS:
 ***************************************************************************
  HISTORIAL DE MODIFICACIONES:
 
- DESCRIPCION:	
- AUTOR:			
- FECHA:		
+ DESCRIPCION:
+ AUTOR:
+ FECHA:
 ***************************************************************************/
 
 DECLARE
@@ -30,12 +30,13 @@ DECLARE
   v_id_unidades				varchar;
   v_id_funcionarios			varchar;
   v_consulta				varchar;
+  v_presupuesta				varchar;
 BEGIN
-  v_nombre_funcion = 'pre.f_get_funcionarios_x_cc';  
-  
+  v_nombre_funcion = 'pre.f_get_funcionarios_x_cc';
+
   v_parametros = pxp.f_get_record(p_tabla);
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'PRE_FUNCCC_SEL'
  	#DESCRIPCION:	Consulta de datos
  	#AUTOR:		Gonzalo Sarmiento Sejas
@@ -43,40 +44,47 @@ BEGIN
 	***********************************/
 
   if(p_transaccion='PRE_FUNCCC_SEL')then
-  
-  select id_uo into v_id_uo_cc
-  from pre.vpresupuesto_cc
-  where gestion=v_parametros.gestion
-  and id_centro_costo = v_parametros.id_cc
-  order by id_centro_costo;
-  
-  --v_id_unidades = orga.f_get_uos_x_gerencia(v_id_uo_cc);
-  v_id_unidades = orga.f_get_uos_x_presupuesto(v_id_uo_cc,'no');
-  
-  v_id_unidades = pxp.concat(v_id_uo_cc::text,v_id_unidades::text); 
 
-  select 
-     pxp.aggarray (asig.id_funcionario) 
-     into v_id_funcionarios
-  from orga.tuo_funcionario asig
-  where 
-         asig.fecha_asignacion <= current_date 
-     and coalesce(asig.fecha_finalizacion, current_date)>=current_date 
-     and asig.estado_reg = 'activo' 
-     and asig.tipo = 'oficial'
-     and asig.id_uo = ANY(regexp_split_to_array(v_id_unidades,',')::integer[]);
+  select pre.id_uo, uo.presupuesta into v_id_uo_cc, v_presupuesta
+  from pre.vpresupuesto_cc pre
+  inner join orga.tuo uo on uo.id_uo=pre.id_uo
+  where pre.gestion=v_parametros.gestion
+  and pre.id_centro_costo = v_parametros.id_cc
+  order by pre.id_centro_costo;
 
-  v_consulta = 'select id_funcionario
-				from orga.vfuncionario
-  				where id_funcionario in ('||v_id_funcionarios||')';
-                
-  v_consulta = regexp_replace(v_consulta,'{','');
-  
-  v_consulta = regexp_replace(v_consulta,'}','');
-  
-  return v_consulta;  
-  
-  /*********************************    
+  IF v_presupuesta = 'si' THEN
+    --v_id_unidades = orga.f_get_uos_x_gerencia(v_id_uo_cc);
+    v_id_unidades = orga.f_get_uos_x_presupuesto(v_id_uo_cc,'no');
+
+    v_id_unidades = pxp.concat(v_id_uo_cc::text,v_id_unidades::text);
+
+    select
+       pxp.aggarray (asig.id_funcionario)
+       into v_id_funcionarios
+    from orga.tuo_funcionario asig
+    where
+           asig.fecha_asignacion <= current_date
+       and coalesce(asig.fecha_finalizacion, current_date)>=current_date
+       and asig.estado_reg = 'activo'
+       and asig.tipo = 'oficial'
+       and asig.id_uo = ANY(regexp_split_to_array(v_id_unidades,',')::integer[]);
+
+    v_consulta = 'select id_funcionario
+                  from orga.vfuncionario
+                  where id_funcionario in ('||v_id_funcionarios||')';
+
+    v_consulta = regexp_replace(v_consulta,'{','');
+
+    v_consulta = regexp_replace(v_consulta,'}','');
+  ELSE
+  	v_consulta = 'select id_funcionario
+                  from orga.vfuncionario
+                  where id_funcionario=0';
+  END IF;
+
+  return v_consulta;
+
+  /*********************************
  	#TRANSACCION:  'PRE_FUNCCC_CONT'
  	#DESCRIPCION:	Consulta de datos
  	#AUTOR:		Gonzalo Sarmiento Sejas
@@ -84,43 +92,50 @@ BEGIN
 	***********************************/
 
   elsif(p_transaccion='PRE_FUNCCC_CONT')then
-  
-  select id_uo into v_id_uo_cc
-  from pre.vpresupuesto_cc
-  where gestion=v_parametros.gestion
-  and id_centro_costo = v_parametros.id_cc
-  order by id_centro_costo;
-  
-  v_id_unidades = orga.f_get_uos_x_presupuesto(v_id_uo_cc,'no');
-  
-  v_id_unidades = pxp.concat(v_id_uo_cc::text,v_id_unidades::text); 
 
-  select 
-     pxp.aggarray (asig.id_funcionario) 
-     into v_id_funcionarios
-  from orga.tuo_funcionario asig
-  where 
-         asig.fecha_asignacion <= current_date 
-     and coalesce(asig.fecha_finalizacion, current_date)>=current_date 
-     and asig.estado_reg = 'activo' 
-     and asig.tipo = 'oficial'
-     and asig.id_uo = ANY(regexp_split_to_array(v_id_unidades,',')::integer[]);
+  select pre.id_uo, uo.presupuesta into v_id_uo_cc, v_presupuesta
+  from pre.vpresupuesto_cc pre
+  inner join orga.tuo uo on uo.id_uo=pre.id_uo
+  where pre.gestion=v_parametros.gestion
+  and pre.id_centro_costo = v_parametros.id_cc
+  order by pre.id_centro_costo;
 
-  v_consulta = 'select count(id_funcionario)
-				from orga.vfuncionario
-  				where id_funcionario in ('||v_id_funcionarios||')';
-                
-  v_consulta = regexp_replace(v_consulta,'{','');
-  
-  v_consulta = regexp_replace(v_consulta,'}','');
-  
-  return v_consulta;  
-  
-  				
+  IF v_presupuesta = 'si' THEN
+    v_id_unidades = orga.f_get_uos_x_presupuesto(v_id_uo_cc,'no');
+
+    v_id_unidades = pxp.concat(v_id_uo_cc::text,v_id_unidades::text);
+
+    select
+       pxp.aggarray (asig.id_funcionario)
+       into v_id_funcionarios
+    from orga.tuo_funcionario asig
+    where
+           asig.fecha_asignacion <= current_date
+       and coalesce(asig.fecha_finalizacion, current_date)>=current_date
+       and asig.estado_reg = 'activo'
+       and asig.tipo = 'oficial'
+       and asig.id_uo = ANY(regexp_split_to_array(v_id_unidades,',')::integer[]);
+
+    v_consulta = 'select count(id_funcionario)
+                  from orga.vfuncionario
+                  where id_funcionario in ('||v_id_funcionarios||')';
+
+    v_consulta = regexp_replace(v_consulta,'{','');
+
+    v_consulta = regexp_replace(v_consulta,'}','');
+  ELSE
+  	v_consulta = 'select count(id_funcionario)
+                  from orga.vfuncionario
+                  where id_funcionario=0';
+  END IF;
+
+  return v_consulta;
+
+
   else
-					     
+
 	raise exception 'Transaccion inexistente';
-					         
+
   end if;
 
 EXCEPTION
